2ef7830b744606cc3fac4bef272fd98e021085a9
jithin <jithinkv06@gmail.com>
2025-03-26 15:00:20 +0530

    modified:   ../src/WebApplication4/Models/TransferData.cs 	../src/.vs/ContainerSolutions/FileContentIndex/9b5d243b-bc02-41e2-9299-2e93e2cc82a2.vsidx

--- src/WebApplication4/Models/TransferData.cs
+++ src/WebApplication4/Models/TransferData.cs
@@ -2,9 +2,11 @@
 {
     public class TransferData
     {
-        public DateTime Date { get; set; }
+        public string Date { get; set; }
 
         public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
 
         public string Summary { get; set; }
     }
